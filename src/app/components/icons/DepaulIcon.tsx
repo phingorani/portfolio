97c8b1d0940f157d2d0381d5cf144bcfa3e06901
pratikhingorani <hingorani.pratik@gmail.com>
2025-11-09 02:55:14 -0600

    stuff

---
+++ src/app/components/icons/DepaulIcon.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+
+export function DepaulIcon() {
+  return (
+    <Image
+      src="/depaul.png"
+      alt="DePaul University Logo"
+      width={24} // Adjust width and height as needed
+      height={24}
+    />
+  );
+}
---
+++ src/app/components/icons/HyattIcon.tsx
@@ -0,0 +1,10 @@
+import { SvgIcon } from '@mui/material';
+
+export function HyattIcon(props: any) {
+  return (
+    <SvgIcon {...props} viewBox="0 0 24 24">
+      {/* CHANGE IT: Add your Hyatt SVG path here */}
+      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" />
+    </SvgIcon>
+  );
+}
